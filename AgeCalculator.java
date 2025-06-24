bac329634c9599a68b4cbbfefdcf5f7b675d494b
keshavgit23 <keshavpawar784@gmail.com>
2025-06-24 23:15:43 +0530

    Create AgeCalculator.java

---
+++ AgeCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class AgeCalculator
+{
+	public static void main (String[] args) 
+	{
+	    Scanner scanner = new Scanner(System.in);
+	
+	System.out.println("Enter Your BirthYear:");
+	int birthYear=scanner.nextInt();
+	
+	System.out.println("Enter The Current Year:");
+    int currentYear=scanner.nextInt();
+    
+    System.out.println("Your Age = " + (currentYear-birthYear));
+    
+    scanner.close();
+	}
+}
